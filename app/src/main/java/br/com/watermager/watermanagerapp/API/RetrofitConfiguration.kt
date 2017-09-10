075a5c5e24b283900df5eb19bf36ccaec1ed8307
Kako Botasso <kakodev@gmail.com>
2017-09-10 03:04:47 -0300

    Implementa login com a API

--- app/src/main/java/br/com/watermager/watermanagerapp/API/RetrofitConfiguration.kt
+++ app/src/main/java/br/com/watermager/watermanagerapp/API/RetrofitConfiguration.kt
@@ -1,17 +1,25 @@
 package br.com.watermager.watermanagerapp.API
 
 import br.com.watermager.watermanagerapp.API.Interfaces.WaterManagerInterface
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitConfiguration {
     companion object {
         val PRODUCTION = "https://watermanager.herokuapp.com"
+        val httpClient = OkHttpClient.Builder()
 
         fun getService(): WaterManagerInterface {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            httpClient.addInterceptor(loggingInterceptor)
+
             val retrofit = Retrofit.Builder()
                     .baseUrl(PRODUCTION)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
                     .build()
 
             return retrofit.create(WaterManagerInterface::class.java)
--- app/src/main/java/br/com/watermager/watermanagerapp/Views/LoginActivity.kt
+++ app/src/main/java/br/com/watermager/watermanagerapp/Views/LoginActivity.kt
@@ -4,24 +4,102 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import br.com.watermager.watermanagerapp.API.Services.LoginService
+import br.com.watermager.watermanagerapp.Models.Login
 import br.com.watermager.watermanagerapp.R
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var etUserLogin: TextView
+    lateinit var etPasswordLogin: TextView
+    lateinit var btLogin: Button
+    lateinit var tvNotYet: TextView
+    lateinit var progressBar: ProgressBar
+    lateinit var username: String
+    lateinit var password: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        prepareLayoutItens()
     }
 
     fun signIn(v: View?) {
-        val mainActivity = Intent(this, MainActivity::class.java)
-        startActivity(mainActivity)
-        finish()
+        hideItensSubmit()
+
+        username = etUserLogin.text.toString()
+        password = etPasswordLogin.text.toString()
+
+        if (validateFields()) {
+            val login = Login()
+            login.username = username
+            login.password = password
+
+            submitForm(login)
+        } else {
+            showItensSubmit()
+        }
     }
 
     fun notYetRegistered(v: View?) {
         val signUpActivity = Intent(this, SignUpActivity::class.java)
         startActivity(signUpActivity)
         finish()
+    }
+
+    private fun prepareLayoutItens() {
+        etUserLogin = findViewById(R.id.et_user_login) as TextView
+        etPasswordLogin = findViewById(R.id.et_password_login) as TextView
+        btLogin = findViewById(R.id.bt_login) as Button
+        tvNotYet = findViewById(R.id.tv_not_yet) as TextView
+        progressBar = findViewById(R.id.progress_bar) as ProgressBar
+        progressBar.visibility = View.GONE
+    }
+
+    private fun validateFields(): Boolean {
+        var valid = true
+        if (username.isEmpty()) {
+            etUserLogin.error = getString(R.string.username_blank_login)
+            valid = false
+        } else if (password.isEmpty()) {
+            etPasswordLogin.error = getString(R.string.password_blank_login)
+            valid = false
+        }
+        return valid
+    }
+
+    private fun hideItensSubmit() {
+        btLogin.visibility = View.GONE
+        tvNotYet.visibility = View.GONE
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun showItensSubmit() {
+        btLogin.visibility = View.VISIBLE
+        tvNotYet.visibility = View.VISIBLE
+        progressBar.visibility = View.GONE
+    }
+
+    private fun submitForm(login: Login) {
+        val loginService = LoginService()
+
+        loginService.loginUser(login, { response ->
+            if( response.isSuccessful ){
+                val mainActivity = Intent(this, MainActivity::class.java)
+                startActivity(mainActivity)
+                finish()
+            } else {
+                Toast.makeText(this, R.string.login_failed, Toast.LENGTH_SHORT).show()
+                showItensSubmit()
+            }
+
+        }, { t ->
+            println(t.message)
+        })
     }
 }
